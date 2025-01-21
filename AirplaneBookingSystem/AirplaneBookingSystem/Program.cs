f39f9ce652118dd64ae3868f27ce60ff2b185331
Ivan Rusev <Ivan.Rusev@indeavr.com>
2025-01-21 15:04:10 +0200

    lock example

---
+++ AirplaneBookingSystem/AirplaneBookingSystem/Program.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Concurrent;
+
+Queue<string> requestQueue = new Queue<string>();
+
+int availableTickets = 10;
+object ticketsLock = new();
+Lock ticketsLockNew = new();
+
+Thread monitoringThread = new Thread(() => MonitorQueue());
+monitoringThread.Start();
+
+Console.WriteLine("Server is running. \r\n Type b to book a ticket. \r\n Type c to cancel ticket. \r\n Type 'exit' to stop.");
+
+while (true)
+{
+    string? input = Console.ReadLine();
+    if (input is null || input == "exit")
+    {
+        break;
+    }
+
+    requestQueue.Enqueue(input);
+}
+
+void MonitorQueue()
+{
+    while (true)
+    {
+        if (requestQueue.TryDequeue(out string? input))
+        {
+            Thread processingThread = new Thread(() => ProcessInput(input));
+            processingThread.Start();
+        }
+        Thread.Sleep(100);
+    }
+}
+
+void ProcessInput(string input)
+{
+    Thread.Sleep(2000);
+
+    lock (ticketsLockNew)
+    {
+
+        if (input == "b")
+        {
+            if (availableTickets > 0)
+            {
+                availableTickets--;
+                Console.WriteLine();
+                Console.WriteLine("Seat is booked. {0} seats are still available", availableTickets);
+            }
+            else
+            {
+                Console.WriteLine("Tickets are not available");
+            }
+        }
+        else if (input == "c")
+        {
+            if (availableTickets < 10)
+            {
+
+                availableTickets++;
+                Console.WriteLine();
+                Console.WriteLine("Seat is cancelled. {0} seats are still available", availableTickets);
+            }
+            else
+            {
+                Console.WriteLine("could not cancel");
+            }
+        }
+    }
+}
